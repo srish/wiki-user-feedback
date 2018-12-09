2570b47cda626a4f36b483df1abffb321a2a62d4
srish <srishakatux@gmail.com>
2018-12-08 22:59:06 -0800

    Do not store userids

--- feedback.js
+++ feedback.js
@@ -5,72 +5,34 @@
  */
 
 $( function () {
-	var yesButton = new OO.ui.ButtonWidget( { label: 'Yes' } );
-	var noButton = new OO.ui.ButtonWidget( { label: 'No' } );
+	var yesButton = new OO.ui.ButtonWidget( { label: 'Yes' } ),
+		noButton = new OO.ui.ButtonWidget( { label: 'No' } ),
+
+		hasUserVoted = false,
+		feedbackData  = [];
 
 	yesButton.on( 'click', function() {
-		modifyVotes( "Yes" );
+		collectVote( "Yes" );
 	} );
 
 	noButton.on( 'click', function() {
-		modifyVotes( "No" );
+		collectVote( "No" );
 	} ); 
 
-	function modifyVotes(response) {
-		var feedbackData = [],
-			hasUserVoted = false,
-			i, 
-			j,
-			pageGotVotes = false,
-			pageID = 11, // TODO: replace value with mw.config.get('wgArticleId')
-			userName = "Tux", // TODO: replace value with mw.config.get('wgUserName') or mw.user.getId()
-			voters = [];
+	function collectVote( response ) {
+		var pageID = 12; // mw.config.get( 'wgArticleId' ); 
 
-
-		if ( feedbackData ) {
-			for ( i = 0; i < feedbackData.length; i++ ) { 
-				if( feedbackData[i] ) {
-					if ( feedbackData[i]['pageId'] == pageID ) {
-						pageGotVotes = true;
-
-						voters = feedbackData[i]['voters'];
-						
-						for ( j = 0; j < voters.length; j++ ) {
-							if ( voters[j] == userName ) {
-								hasUserVoted = true;
-								break;
-							}
-						}
-						
-						if( !hasUserVoted && response == "Yes" ) {
-							var votes = feedbackData[i]['votes'];
-
-							if( !votes )
-								votes = 0;
-
-							feedbackData[i]['votes'] = votes + 1;
-							voters.push( userName );
-						}
-
-						if( hasUserVoted && response == "No" ) {
-							if ( feedbackData[i]['votes'] > 0 ) {
-								feedbackData[i]['votes'] = feedbackData[i]['votes'] - 1;
-								voters.splice( userName );
-							}
-						}
-						
-						break;
-					}
-				}
-			}
+		if( !hasUserVoted && response == "Yes" ) {
+			hasUserVoted  = true;
+			feedbackData.push( { pageId: pageID, vote: "Yes" } );	
 		}
 
-		if( !pageGotVotes && response == "Yes" ) {
-			feedbackData.push( { pageId: pageID, votes: 1, voters: [userName] } );
+		if( hasUserVoted && response == "No" ) {
+			hasUserVoted  = false;
+			feedbackData.push( { pageId: pageID, vote: "No" } );
 		}
 
-		console.log(feedbackData);
-
+		console.log( feedbackData );
 	}
 
 	//TODO: Replace `body` with `#mw-content-text`
