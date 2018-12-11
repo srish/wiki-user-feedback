74c48fe63f776b9fe70a1c16f7eaf4dcab1b9325
srish <srishakatux@gmail.com>
2018-12-10 17:23:18 -0800

    Use oojs-ui-core; display form after catlinks if it exists

--- feedback.js
+++ feedback.js
@@ -4,39 +4,58 @@
  * Displays a feedback form and collect user votes   
  */
 
-$( function () {
-	var yesButton = new OO.ui.ButtonWidget( { label: 'Yes' } ),
-		noButton = new OO.ui.ButtonWidget( { label: 'No' } ),
+mw.loader.using( 'oojs-ui-core' ).done( function () {
+	$( function () {
+		var yesButton = new OO.ui.ButtonWidget( { label: 'Yes' } ),
+			noButton = new OO.ui.ButtonWidget( { label: 'No' } ),
+	
+			hasUserVoted = false,
+			feedbackData  = [],
+			apiUrl = mw.config.get( 'wgServer' ) + '/w/api.php',
+			curPageId = mw.config.get( 'wgArticleId' );
+		
+		$.getJSON( apiUrl, {
+			action: "query",
+			format: "json",
+			pageids: curPageId,
+			prop: "categories"
+		} ).
+		done( function( data ) {
+			var cats = data.query.pages[curPageId].categories;
 
-		hasUserVoted = false,
-		feedbackData  = [];
+			if( cats && cats.length >= 0 ) {
+				$( '#catlinks' ).after( noButton.$element );
+				$( '#catlinks' ).after( yesButton.$element );
+				$( '#catlinks' ).after( 'Was this documentation helpful? ' );
+				$( '#catlinks' ).after( '<br>' );
+			} else {
+				$( '#mw-content-text' ).after( noButton.$element );
+				$( '#mw-content-text' ).after( yesButton.$element );
+				$( '#mw-content-text' ).after( 'Was this documentation helpful? ' );
+				$( '#mw-content-text' ).after( '<br>' );
+			}
+		} );
 
-	yesButton.on( 'click', function() {
-		collectVote( "Yes" );
+		yesButton.on( 'click', function() {
+			collectVote( "Yes" );
+		} );
+	
+		noButton.on( 'click', function() {
+			collectVote( "No" );
+		} );
+	
+		function collectVote( response ) {
+			if( !hasUserVoted && response == "Yes" ) {
+				hasUserVoted  = true;
+				feedbackData.push( { page_id: curPageId, vote: "Yes" } );
+			}
+	
+			if( hasUserVoted && response == "No" ) {
+				hasUserVoted  = false;
+				feedbackData.push( { page_id: curPageId, vote: "No" } );
+			}
+
+			console.log( feedbackData );
+		}
 	} );
-
-	noButton.on( 'click', function() {
-		collectVote( "No" );
-	} ); 
-
-	function collectVote( response ) {
-		var pageID = 12; // mw.config.get( 'wgArticleId' ); 
-
-		if( !hasUserVoted && response == "Yes" ) {
-			hasUserVoted  = true;
-			feedbackData.push( { pageId: pageID, vote: "Yes" } );	
-		}
-
-		if( hasUserVoted && response == "No" ) {
-			hasUserVoted  = false;
-			feedbackData.push( { pageId: pageID, vote: "No" } );
-		}
-
-		console.log( feedbackData );
-	}
-
-	//TODO: Replace `body` with `#mw-content-text`
-	$( 'body' ).append( 'Was this documentation helpful? ' );
-	$( 'body' ).append( yesButton.$element );
-	$( 'body' ).append( noButton.$element );
 } );
