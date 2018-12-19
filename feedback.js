2fde135be74500649549fd54bde225f94afa5d6e
srish <srishakatux@gmail.com>
2018-12-18 17:40:47 -0800

    Support voting only once, display confirmation msg & limitations to when and where to embed form * Supporting voting only once with cookie handling * Show confirmation message to a user when they vote * Show feedback form only in a requested namespace and article page * Code for logging event; commented right now

--- feedback.js
+++ feedback.js
@@ -3,46 +3,131 @@
  * 
  * Displays a feedback form and collect user votes   
  */
+mw.loader.using( 'oojs-ui-core', 'ext.eventLogging' ).done( function() {
+    var feedbackData = [],
+        title = mw.config.get( 'wgTitle' ),
+        articleId = mw.config.get( 'wgArticleId' ),
+        namespace = mw.config.get( 'wgCanonicalNamespace' ),
+        namespaceNum = mw.config.get( 'wgNamespaceNumber' ),
+        action = mw.config.get( 'wgAction' ),
+        talkPage = mw.Title.newFromText( title, namespaceNum ).getTalkPage(),
+        talkPageUrl = talkPage.getUrl(),
+        supportedNamespaces = ["User"],
+        display = '#catlinks',
+        prevVote = '';
 
-mw.loader.using( 'oojs-ui-core' ).done( function () {
-	$( function () {
-		var yesButton = new OO.ui.ButtonWidget( { label: 'Yes' } ),
-			noButton = new OO.ui.ButtonWidget( { label: 'No' } ),
-	
-			hasUserVoted = false,
-			feedbackData  = [],
-			curPageId = mw.config.get( 'wgArticleId' ),
-			displayAfter = '#catlinks';
-		
-		if ( $( displayAfter ).length == 0 ) {
-			displayAfter = '#mw-conent-text';
-		}
+    function getPreviousVote() {
+        var start,
+            end,
+            cookie = document.cookie;
 
-		$( displayAfter ).after( noButton.$element );
-		$( displayAfter ).after( yesButton.$element );
-		$( displayAfter ).after( 'Was this documentation helpful? ' );
-		$( displayAfter ).after( '<br>' );
+        if ( cookie ) {
+            start = cookie.search( 'vote=' );
+            end = cookie.search( ';' );
 
-		yesButton.on( 'click', function() {
-			collectVote( "Yes" );
-		} );
-	
-		noButton.on( 'click', function() {
-			collectVote( "No" );
-		} );
-	
-		function collectVote( response ) {
-			if( !hasUserVoted && response == "Yes" ) {
-				hasUserVoted  = true;
-				feedbackData.push( { page_id: curPageId, vote: "Yes" } );
-			}
-	
-			if( hasUserVoted && response == "No" ) {
-				hasUserVoted  = false;
-				feedbackData.push( { page_id: curPageId, vote: "No" } );
-			}
+            if ( start !== -1 ) {
+                cookie = cookie.substring( start + 9, end );
+                cookie = cookie.substring( 5, cookie.length );
 
-			console.log( feedbackData );
-		}
-	} );
+                return cookie;
+            }
+        }
+
+        return null;
+    }
+
+    function collectVote( response ) {
+        if ( getPreviousVote() ) {
+            return;
+        }
+
+        if ( response == "yes" ) {
+            feedbackData.push( {
+                page_id: articleId,
+                vote: "yes"
+            } );
+
+            /*
+             * [TODO] Replace the above line with > mw.eventLog.logEvent(  
+             * 'UserFeedback', { page_id: articleId, vote: "Yes" }  );
+             */
+        }
+
+        if ( response == "no" ) {
+            feedbackData.push( {
+                page_id: articleId,
+                vote: "no"
+            } );
+
+            /*
+             * [TODO] Replace the above line with > //mw.eventLog.logEvent(  
+             * 'UserFeedback', { page_id: articleId, vote: "no" }  );
+             */
+        }
+
+        setCookie( response );
+        showConfirmationMsg( response );
+        console.log( feedbackData );
+    }
+
+    function setCookie( response ) {
+        document.cookie = "vote=" + response + ";";
+    }
+
+    function showConfirmationMsg( response ) {
+        $( "#doc-feedback-form" ).empty();
+
+        if ( response == "yes" ) {
+            $( "#doc-feedback-form" ).append( "<span> Thank you for the" +
+                " feedback. We are glad that you found the documentation" +
+                " useful! </span>" );
+        }
+
+        if ( response == "no" ) {
+            $( "#doc-feedback-form" ).append( "<span> Thank you for the" +
+                " feedback. It would be helpful if you could provide more" +
+                " information on what needs improvement on the article's" +
+                " <a href='" + talkPageUrl + "' target='blank'>" +
+                "talk page.</a> </span>" );
+        }
+    }
+
+    $( document ).ready( function() {
+        var yesButton = new OO.ui.ButtonWidget( {
+                label: 'Yes'
+            } ),
+            noButton = new OO.ui.ButtonWidget( {
+                label: 'No'
+            } );
+
+        if ( action !== "view" ||
+            namespace.indexOf( supportedNamespaces ) == -1 ) {
+            return;
+        }
+        if ( $( display ).length == 0 ) {
+            display = '#mw-content-text';
+        }
+        $( display ).after( "<div id='doc-feedback-form'></div>" );
+
+        prevVote = getPreviousVote();
+
+        if ( prevVote == "yes" || prevVote == "no" ) {
+            showConfirmationMsg( prevVote );
+        } else {
+            $( "#doc-feedback-form" ).append( '<span>' +
+                'Was this documentation helpful? </span>' );
+            $( "#doc-feedback-form" ).append( yesButton.$element );
+            $( "#doc-feedback-form" ).append( noButton.$element );
+        }
+
+        yesButton.on( 'click', function(  ) {
+            collectVote( "yes" );
+        } );
+
+        noButton.on( 'click', function(  ) {
+            collectVote( "no" );
+        } );
+
+    } );
+
 } );
